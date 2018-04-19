f37733d06190bdfbd4b339cda2c3cfcaf1a354ec
idelfrides <idelfridesjorge@alu.ufc.br>
2018-04-19 20:55:24 -0300

    menu desconfigurado

--- js/sticky-navbar-effect.js
+++ js/sticky-navbar-effect.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {wmFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -7,8 +7,10 @@ var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+
+/* Add the sticky class to the navbar when you reach its scroll position.
+   Remove "sticky" when you leave the scroll position */
+function wmFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
